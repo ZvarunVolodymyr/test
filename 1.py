3d975271312dd493125ff54b25cd23e6819a0b81
ZvarunVolodymyr <90132347+ZvarunVolodymyr@users.noreply.github.com>
2021-09-06 14:01:41 +0300

    Add files via upload
    1 лекція

---
+++ 1.py
@@ -0,0 +1,71 @@
+# ЛЕКЦІЯ 1
+
+
+# PEP8 - оформлення коду
+# документація
+
+
+# jupyter notebook
+
+
+# a = 5
+# b = 5
+# print(id(a), id(b))
+# b = 6
+# print(id(a), id(b))
+# b = 5
+# print(id(b))
+
+
+# print(2**100000)
+
+
+# import sys
+# print(sys.getsizeof(0))
+
+
+# import decimal
+# import fractions
+# Null = None
+# Int = int(2)
+# Bool = bool(True)
+# Float = float(0.003)
+# Complex = complex()
+# Decimal = decimal.Decimal
+# Decimal = 0.003
+# Fraction = fractions.Fraction
+# Fraction = 1/3 + 2/3
+# Str = str('string')
+# print(Null, Int, Bool, Float, Decimal, Fraction, Str)
+
+
+# List = ['l', 'i', 's', 't']
+# Tuple = ('t', 'u', 'p', 'l', 'e')
+# Dict = {1 : 'd', 2 : 'i', 3 : 'c', 4 : 't'}
+# Set = set('ssssetttt') #?
+# print(List, Tuple, Dict, Set)
+
+
+# a : int = 5
+# print(a)
+
+
+# x = 2 + \
+#     3
+# print(x)
+
+
+# name, age = 'name', 0
+# print(name, age)
+
+
+# x = 1
+#  y = 2
+
+
+# українська = 1
+# print(українська)
+
+
+# нема констант
+
